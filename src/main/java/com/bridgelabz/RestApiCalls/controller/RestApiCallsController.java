620005d487bfee441b2c41c765e40257ec430d8d
sheetal-10 <sheetalp257@gmail.com>
2021-10-10 23:53:21 +0530

    Added: query param to display message

--- src/main/java/com/bridgelabz/RestApiCalls/controller/RestApiCallsController.java
+++ src/main/java/com/bridgelabz/RestApiCalls/controller/RestApiCallsController.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.RestApiCalls.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestApiCallsController {
     @GetMapping("test1")
     public String getMessage() {
         return "Hello from Bridgelabz";
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String getMessage(@RequestParam String name) {
+        return "Hello : " + name + " from bridgelabz ";
     }
 }
